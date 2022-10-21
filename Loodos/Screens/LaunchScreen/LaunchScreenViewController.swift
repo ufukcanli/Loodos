cae90e347a479d281977c880bf447450e4382a46
Ufuk Canlı <ufuk.canli@rapsodo.com>
2022-10-21 16:35:04 +0300

    Add title label

--- Loodos/Screens/LaunchScreen/LaunchScreenViewController.swift
+++ Loodos/Screens/LaunchScreen/LaunchScreenViewController.swift
@@ -9,9 +9,30 @@ import UIKit
 
 final class LaunchScreenViewController: UIViewController {
     
+    private lazy var titleLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
         view.backgroundColor = .systemBlue
+        
+        configureTitleLabel()
+    }
+}
+
+private extension LaunchScreenViewController {
+    
+    func configureTitleLabel() {
+        titleLabel.text = "Loodos"
+        titleLabel.textColor = .systemYellow
+        titleLabel.font = .systemFont(ofSize: 35, weight: .medium)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
